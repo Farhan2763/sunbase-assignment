9532e471e4ee1fbe7a07a05ba1a20b9b43eeef06
farhan.2763 <farhan2763@gmail.com>
2024-06-12 17:16:33 +0530

    initial message

---
+++ src/main/java/com/customer/controller/Edit.java
@@ -0,0 +1,205 @@
+package com.customer.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.customer.DAOImpl.CustomerDAOImpl;
+import com.customer.model.Customer;
+import com.customer.util.CustomerList2;
+
+@WebServlet("/edit")
+public class Edit extends HttpServlet {
+
+	static private CustomerDAOImpl customerDAOImpl;
+	static private List<Customer> allCustomer;
+
+	@Override
+	public void init() throws ServletException {
+		customerDAOImpl = new CustomerDAOImpl();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.setContentType("text/html");
+		HttpSession session = req.getSession();
+		allCustomer = (List<Customer>) session.getAttribute("Allcustomer");
+		if (allCustomer == null) {
+			allCustomer = customerDAOImpl.getAllCustomer();
+			session.setAttribute("Allcustomer", allCustomer);
+
+			req.getRequestDispatcher("home.jsp").include(req, resp);
+			return;
+		}
+
+		String action = req.getParameter("action");
+		if (action != null) {
+			if (action.equals("edit")) {
+				edit(req, resp);
+			} else if (action.equals("Update")) {
+				update(req, resp, session);
+			} else if (action.equals("delete")) {
+				delete(req, resp, session);
+			} else if (action.equals("Add")) {
+				add(req, resp, session);
+			} else if (action.equals("search")) {
+				search(req, resp, session);
+			} else if (action.equals("sync")) {
+				sync(session);
+			}
+		}
+		session.setAttribute("Allcustomer", allCustomer);
+		req.getRequestDispatcher("home.jsp").include(req, resp);
+	}
+
+	private void sync(HttpSession session) {
+
+		String authtoken = (String) session.getAttribute("Admin");
+		JSONArray getcustomerList = CustomerList2.getcustomerList(authtoken);
+		for (int i = 0; i < getcustomerList.length(); i++) {
+			JSONObject jsonObject = getcustomerList.getJSONObject(i);
+			String id = jsonObject.getString("uuid");
+			String fname = jsonObject.getString("first_name");
+			String lname = jsonObject.getString("last_name");
+			String street = jsonObject.getString("street");
+			String address = jsonObject.getString("address");
+			String city = jsonObject.getString("city");
+			String state = jsonObject.getString("state");
+			String email = jsonObject.getString("email");
+			String phone = jsonObject.getString("phone");
+
+			Customer customerById = customerDAOImpl.getCustomerById(id);
+			Customer customer = new Customer(id, fname, lname, street, address, city, state, email, phone);
+			session.setAttribute("message", "All Customers Synced Successfully");
+			if (customerById == null) {
+				customerDAOImpl.addCustomer(customer);
+			} else {
+				customerDAOImpl.updateCustomer(customer);
+			}
+		}
+		allCustomer = customerDAOImpl.getAllCustomer();
+	}
+
+	private void search(HttpServletRequest req, HttpServletResponse resp, HttpSession session) {
+
+		String searchby = req.getParameter("searchby");
+		String searchInput = req.getParameter("searchInput");
+		if (searchby.equals("First Name")) {
+
+			allCustomer = customerDAOImpl.getCustomerByFirstName(searchInput);
+			if (allCustomer.isEmpty() || allCustomer == null) {
+				session.setAttribute("message"," No such " + searchby + " exist please try with different " + searchby);
+			}
+			
+
+		} else if (searchby.equals("City")) {
+			allCustomer = customerDAOImpl.getCustomerByCity(searchInput);
+			if (allCustomer.isEmpty() || allCustomer == null) {
+				session.setAttribute("message"," No such " + searchby + " exist please try with different " + searchby);
+			}
+			
+
+		} else if (searchby.equals("Email")) {
+			allCustomer = customerDAOImpl.getCustomerByEmail(searchInput);
+			if (allCustomer.isEmpty() || allCustomer == null) {
+				session.setAttribute("message"," No such " + searchby + " exist please try with different " + searchby);
+			}
+			
+
+		} else if (searchby.equals("Phone")) {
+			allCustomer = customerDAOImpl.getCustomerByPhone(searchInput);
+			if (allCustomer.isEmpty() || allCustomer == null) {
+				session.setAttribute("message"," No such " + searchby + " number exist please try with different " + searchby + " number");
+			}
+			
+
+		}
+	}
+
+	private void add(HttpServletRequest req, HttpServletResponse resp, HttpSession session)
+			throws ServletException, IOException {
+
+		String id = req.getParameter("Cid");
+		String FirstName = req.getParameter("First Name");
+		String LastName = req.getParameter("Last Name");
+		String Street = req.getParameter("Street");
+		String Address = req.getParameter("Address");
+		String City = req.getParameter("City");
+		String State = req.getParameter("State");
+		String Email = req.getParameter("Email");
+		String Phone = req.getParameter("Phone");
+		Customer customer = new Customer(id, FirstName, LastName, Street, Address, City, State, Email, Phone);
+		int i = customerDAOImpl.addCustomer(customer);
+		if (i == 0) {
+			session.setAttribute("message", " We have Some technical issue please try after some time");
+		} else {
+
+			System.out.println(i);
+			allCustomer = customerDAOImpl.getAllCustomer();
+			session.setAttribute("message", "Customer " + FirstName + " Added Successfully");
+		}
+	}
+
+	private void delete(HttpServletRequest req, HttpServletResponse resp, HttpSession session)
+			throws ServletException, IOException {
+
+		String id = req.getParameter("id");
+		customerDAOImpl = new CustomerDAOImpl();
+		Customer customerById = customerDAOImpl.getCustomerById(id);
+		int i = customerDAOImpl.deleteCustomer(id);
+		if (i == 0) {
+			session.setAttribute("message", "Customer dosen't exist please refresh it");
+		} else {
+
+			System.out.println(i);
+			allCustomer = customerDAOImpl.getAllCustomer();
+			session.setAttribute("message", "Customer " + customerById.getFname() + " Deleted Successfully");
+		}
+
+//		req.getRequestDispatcher("home.jsp").include(req, resp);
+	}
+
+	private void update(HttpServletRequest req, HttpServletResponse resp, HttpSession session)
+			throws ServletException, IOException {
+
+		String FirstName = req.getParameter("First Name");
+		String LastName = req.getParameter("Last Name");
+		String Street = req.getParameter("Street");
+		String Address = req.getParameter("Address");
+		String City = req.getParameter("City");
+		String State = req.getParameter("State");
+		String Email = req.getParameter("Email");
+		String Phone = req.getParameter("Phone");
+		String id = req.getParameter("uid");
+		Customer customer = new Customer(id, FirstName, LastName, Street, Address, City, State, Email, Phone);
+		int i = customerDAOImpl.updateCustomer(customer);
+		if (i == 0) {
+			session.setAttribute("message", "Customer dosen't exist please refresh it");
+		} else {
+
+			System.out.println(i);
+			allCustomer = customerDAOImpl.getAllCustomer();
+			session.setAttribute("message", "Customer " + FirstName + " Updated Successfully");
+		}
+	}
+
+	private void edit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String id = req.getParameter("id");
+		System.out.println(id);
+		Customer customer = customerDAOImpl.getCustomerById(id);
+//	System.out.println(customer);
+
+		req.setAttribute("customer", customer);
+		req.getRequestDispatcher("editCustomer.jsp").forward(req, resp);
+	}
+}
---
+++ src/main/webapp/home.jsp
@@ -0,0 +1,173 @@
+
+<%@page import="com.customer.DAOImpl.CustomerDAOImpl"%>
+<%@ page import="java.util.List"%>
+<%@ page import="com.customer.DAO.CustomerDAO"%>
+<%@ page import="com.customer.model.Customer"%>
+<%@ page contentType="text/html;charset=UTF-8" language="java"%>
+<html>
+<head>
+<title>Customer Details</title>
+<link rel="stylesheet" type="text/css" href="cust.css">
+</head>
+<body>
+	<h2>Customer List</h2>
+	<%
+	String message = (String)session.getAttribute("message");
+	if(message!=null && !message.isEmpty()){
+		%>
+		<h3 style="color: blue"><%=message %></h3>
+		<%
+	}session.setAttribute("message",null);
+	%>
+
+	<%
+	List<Customer> allcustomers = (List<Customer>) session.getAttribute("Allcustomer");
+	/*CustomerDAOImpl CustomerDAOImpl = new  CustomerDAOImpl();
+	 List<Customer> allcustomers = CustomerDAOImpl.getAllCustomer(); 
+	// Fetch all customers
+	 List<Customer> customers = customerDAO.getAllCustomer(); */
+
+	// Check if the list is not empty
+	String admin = (String) session.getAttribute("Admin");
+
+	if (allcustomers != null) {
+		// Pagination parameters
+		int currentPage = 1;
+		int recordsPerPage = 5;
+
+		if (request.getParameter("page") != null) {
+			currentPage = Integer.parseInt(request.getParameter("page"));
+		}
+
+		int startRecord = (currentPage - 1) * recordsPerPage;
+		int endRecord = Math.min(startRecord + recordsPerPage, allcustomers.size());
+
+		List<Customer> customers1 = allcustomers.subList(startRecord, endRecord);
+
+		if (admin != null) {
+	%>
+	<div class="add-customer-btn">
+		<a href="Add.jsp"><button id="add-customer-btn">Add
+				Customer</button></a> 
+				<a href="edit?&action=sync"><input type="submit" value="Sync"></a>
+				<a href="logout"><input type="submit" value="logut" class=""></a>
+	</div>
+	<div class="search-dropdown">
+		<form action="edit">
+			<input type="hidden" name="action" value="search"> <select
+				id="search-dropdown" name="searchby" onchange="showSearchBox()">
+				<option value="" >Select Search By</option>
+				<option value="First Name">First Name</option>
+				<option value="City">City</option>
+				<option value="Email">Email</option>
+				<option value="Phone">Phone</option>
+			</select> <input type="text" name="searchInput" id="search-input"
+				placeholder="Enter search term"> 
+				<input type="submit" value="Search">
+		</form>
+	</div>
+
+	<table>
+		<tr>
+			
+			<th>First Name</th>
+			<th>Last Name</th>
+			<th>Address</th>
+			<th>City</th>
+			<th>State</th>
+			<th>Email</th>
+			<th>Phone</th>
+			<th>Action</th>
+		</tr>
+		<%
+		for (Customer customer : customers1) {
+		%>
+		<tr>
+			
+			<td><%=customer.getFname()%></td>
+			<td><%=customer.getLname()%></td>
+			<td><%=customer.getAddress()%></td>
+			<td><%=customer.getCity()%></td>
+			<td><%=customer.getState()%></td>
+			<td><%=customer.getEmail()%></td>
+			<td><%=customer.getPhone()%></td>
+			<td class="action-buttons"><a
+				href="edit?id=<%=customer.getId()%>&action=edit" class="edit-btn">Edit</a>
+				<a href="edit?id=<%=customer.getId()%>&action=delete"><input
+					type="submit" value="Delete" class="delete-btn"
+					onclick="return confirm('Are you sure you want to delete this customer?');"></a></td>
+
+		</tr>
+		
+		<%
+		}
+		%>
+	</table>
+	
+	<!-- Pagination links -->
+	<!-- Pagination links -->
+	<div class="pagination">
+		<%-- Previous button --%>
+		<%
+		if (currentPage > 1) {
+		%>
+		<a href="home.jsp?page=<%=currentPage - 1%>">Previous</a>
+		<%
+		}
+		%>
+
+		<%-- Page numbers --%>
+		<%
+		for (int i = 1; i <= Math.ceil((double) allcustomers.size() / recordsPerPage); i++) {
+		%>
+		<%
+		if (i == currentPage) {
+		%>
+		<span class="current-page"><%=i%></span>
+		<%
+		} else {
+		%>
+		<a href="home.jsp?page=<%=i%>"><%=i%></a>
+		<%
+		}
+		%>
+		<%
+		}
+		%>
+
+		<%-- Next button --%>
+		<%
+		if (currentPage < Math.ceil((double) allcustomers.size() / recordsPerPage)) {
+		%>
+		<a href="home.jsp?page=<%=currentPage + 1%>">Next</a>
+		<%
+		}
+		%>
+	</div>
+	<%
+	} else if (allcustomers == null && admin == null) {
+	%>
+	<p>
+		Not Authorized, Please <a href="Login.jsp">Login.</a>
+	</p>
+
+	<%
+	} else {
+	%>
+	<p>
+		No customers found.<a href="Login.jsp">Login</a>
+	</p>
+	<%
+	}
+	} else {
+	%>
+	<p>
+		No customers found, please <a href="Login.jsp">Login.</a>
+	</p>
+	<%
+	}
+	%>
+
+	
+</body>
+</html>
